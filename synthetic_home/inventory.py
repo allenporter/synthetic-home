0249e6da9d5bf51ac9f0577121495bbf2db596e3
Allen Porter <allen.porter@gmail.com>
2024-06-01 22:31:03 +0000

    Delete unused DeviceInfo

--- synthetic_home/inventory.py
+++ synthetic_home/inventory.py
@@ -53,24 +53,6 @@ class Area(DataClassYAMLMixin):
 
 
 @dataclass
-class DeviceInfo(DataClassYAMLMixin):
-    """Device model information."""
-
-    model: str | None = None
-    """The model name of the device e.g. 'Learning Thermostat'."""
-
-    manufacturer: str | None = None
-    """The manufacturer of the device e.g. 'Nest'."""
-
-    sw_version: str | None = None
-    """The firmware version string of the device e.g. '1.0.2'."""
-
-    class Config(BaseConfig):
-        code_generation_options = ["TO_DICT_ADD_OMIT_NONE_FLAG"]
-        sort_keys = False
-
-
-@dataclass
 class Device(DataClassYAMLMixin):
     """Represents a devie within a home."""
 
