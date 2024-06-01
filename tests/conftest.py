3ca70c95e3a8fe1944b5c30696197437d50d11e4
Allen Porter <allen.porter@gmail.com>
2024-06-01 22:56:19 +0000

    Add snapshot test fixture

---
+++ tests/conftest.py
@@ -0,0 +1,29 @@
+"""Global fixtures for Synthetic Home tests."""
+
+import pathlib
+
+import pytest
+from syrupy import SnapshotAssertion
+
+from syrupy.extensions.amber import AmberSnapshotExtension
+from syrupy.location import PyTestLocation
+
+
+DIFFERENT_DIRECTORY = "snapshots"
+
+
+class DifferentDirectoryExtension(AmberSnapshotExtension):
+    """Extension to set a different snapshot directory."""
+
+    @classmethod
+    def dirname(cls, *, test_location: "PyTestLocation") -> str:
+        """Override the snapshot directory name."""
+        return str(
+            pathlib.Path(test_location.filepath).parent.joinpath(DIFFERENT_DIRECTORY)
+        )
+
+
+@pytest.fixture
+def snapshot(snapshot: SnapshotAssertion) -> SnapshotAssertion:
+    """Fixture to override the snapshot directory."""
+    return snapshot.use_extension(DifferentDirectoryExtension)
